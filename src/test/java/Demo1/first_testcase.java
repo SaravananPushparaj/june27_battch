9ccb5825b4f5a30bb68734fb73e0a3fb531f1718
saravananpushparaj <saravananpushparaj@gmail.com>
2016-06-26 12:24:19 +0530

    V1.0

---
+++ src/test/java/Demo1/first_testcase.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+public class first_testcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
